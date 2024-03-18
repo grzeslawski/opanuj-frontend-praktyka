922c13f6d538c5d06a68b948ef5c203672ac8657
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2024-03-18 21:08:54 +0100

    added axios interceptor

--- examples/module1/lesson4/axios-interceptor/index.js
+++ examples/module1/lesson4/axios-interceptor/index.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 // Add a request interceptor
 axios.interceptors.request.use(function (config) {
+  config.metadata = { startTime: new Date() };
   return config;
 });
 
 // Add a response interceptor
 axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  const endTime = new Date();
+  const duration = endTime - response.config.metadata.startTime;
+  console.log(
+    `Czas oczekiwania na odpowiedź dla: ${response.config.url} wyniósł: ${duration}ms`
+  );
   return response;
 });
 
