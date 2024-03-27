e5baa2ba39e9945a6ace74372c1a301b1f5de492
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2024-03-27 21:02:57 +0100

    added more tests to validator

--- examples/module2/lesson1/discount-validator/validator.test.ts
+++ examples/module2/lesson1/discount-validator/validator.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { formValidator } from './validator';
+import { error } from 'console';
 
 describe('Form validation', () => {
     test('should return an error if first name is missing', () => {
@@ -16,4 +17,23 @@ describe('Form validation', () => {
         const errors = formValidator('John', 'Doe', -1);
         expect(errors).toContain('Age must be a positive number');
     });
+
+    test('should return nothing when valid data has been passed', () => {
+        const errors = formValidator('John', 'Doe', 18);
+        expect(errors).toHaveLength(0);
+    })
+
+    test('firstName and lastName should have more than 1 character', () => {
+        const errors = formValidator('J', 'D', 18);
+        expect(errors).toHaveLength(2);
+    })
+
+    test('should throw error when age is not a number', () => {
+        const testFunction = () => {
+            formValidator('John', 'Doe', 'age');
+        };
+        
+        expect(testFunction).toThrow('age should be a number');
+
+    })
 });
--- examples/module2/lesson1/discount-validator/validator.ts
+++ examples/module2/lesson1/discount-validator/validator.ts
@@ -5,16 +5,20 @@ export function formValidator(
 ) {
   const errors: string[] = [];
 
-  if (!firstName) {
+  if (firstName.length <= 1 ) {
     errors.push('First name is required');
   }
 
-  if (!lastName) {
+  if (lastName.length <= 1) {
     errors.push('Last name is required');
   }
 
   if (age < 0) {
     errors.push('Age must be a positive number');
+  }
+
+  if (typeof age !== 'number') {
+    throw new Error('age should be a number')
   }
 
   return errors;
